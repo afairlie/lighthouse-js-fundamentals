b2f902c5a5144ccb3e859b0d3fa58ced8de575ec
afairlie <ariane.fairlie@gmail.com>
2020-01-27 23:12:17 +0000

    loopy assignment

---
+++ loopy.js
@@ -0,0 +1,11 @@
+for (let loop = 100; loop <= 200; loop++){
+  if (loop%3 === 0 && loop%4 === 0){
+    console.log("LoopyLighthouse");
+  } else if (loop%3 === 0){
+    console.log("Loopy");
+  } else if (loop%4 === 0){
+    console.log("Lighthouse");
+  } else {
+    console.log(loop);
+  }
+}

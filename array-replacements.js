40aea077e6b748a46a4a30e35a9be4981efb66d4
afairlie <ariane.fairlie@gmail.com>
2020-02-07 18:04:46 +0000

    my own experiment with replacing elements in an array

---
+++ array-replacements.js
@@ -0,0 +1,7 @@
+const fruits = ["apple", "orange", "pear", "fig"];
+
+console.log(fruits);
+
+fruits[0, 2] = "grapefruit", "pomegranite"; //adds "grapefruit" to index 2 (doesn't read 0, or "pomegranite")
+
+console.log(fruits);

d9094c591db916ce15502340f8d895812570309d
afairlie <ariane.fairlie@gmail.com>
2020-03-16 17:42:38 +0000

    js fundamentals kata, count times element occurs in array, return result based on threshold

---
+++ check-air.js
@@ -0,0 +1,26 @@
+const checkAir = function (samples, threshold) {
+  let dirtyCount = 0;
+
+  for (let sample of samples){
+    sample === 'dirty' ? dirtyCount++
+    : false;
+  }
+  
+  return (dirtyCount / samples.length) < threshold ? "Clean"
+  : "Polluted";
+};
+
+console.log(checkAir(
+  ['clean', 'clean', 'dirty', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'],
+  0.3
+));
+
+console.log(checkAir(
+  ['dirty', 'dirty', 'dirty', 'dirty', 'clean'],
+  0.25
+));
+
+console.log(checkAir(
+  ['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'],
+  0.9
+))

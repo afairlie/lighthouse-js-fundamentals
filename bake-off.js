c8517e12014e12fc5a82156654a7f64811235bdf
afairlie <ariane.fairlie@gmail.com>
2020-03-18 20:03:41 +0000

    check if every ingredient from a recipe in an array of recipes is included in an array of bakery ingredients

---
+++ bake-off.js
@@ -0,0 +1,50 @@
+const chooseRecipe = function(bakeryA, bakeryB, recipes) {
+  const allBakeryIngredients = bakeryA.concat(bakeryB); // merge bakery ingredient arrays
+
+  for (let recipe of recipes){ // for each recipe in recipes array
+
+    if (recipe.ingredients.every(o => allBakeryIngredients.includes(o))){ // if every ingredient in current recipe is included in bakery ingredients array
+      return recipe.name; 
+    }
+  }
+
+}
+
+let bakeryA = ['saffron', 'eggs', 'tomato paste', 'coconut', 'custard'];
+let bakeryB = ['milk', 'butter', 'cream cheese'];
+let recipes = [
+    {
+        name: 'Coconut Sponge Cake',
+        ingredients: ['coconut', 'cake base']
+    },
+    {
+        name: 'Persian Cheesecake',
+        ingredients: ['saffron', 'cream cheese']
+    },
+    {
+        name: 'Custard Surprise',
+        ingredients: ['custard', 'ground beef']
+    }
+];
+
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+
+bakeryA = ['potatoes', 'bay leaf', 'raisins'];
+bakeryB = ['red bean', 'dijon mustard', 'apples'];
+recipes = [
+    {
+        name: 'Potato Ganache',
+        ingredients: ['potatoes', 'chocolate']
+    },
+    {
+        name: 'Sweet Fish',
+        ingredients: ['anchovies', 'honey']
+    },
+    {
+        name: "Nima's Famous Dijon Raisins",
+        ingredients: ['dijon mustard', 'raisins']
+    }
+];
+
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+

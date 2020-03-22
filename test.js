8f489912bb1129ef4320458844d9b9931b737e64
afairlie <ariane.fairlie@gmail.com>
2020-03-22 18:18:55 +0000

    square-code draft functions

--- test.js
+++ test.js
@@ -1,77 +1,36 @@
-let arr = [     // TEST ARRAY
-  [0, 0, 0, 0, 0, 1, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [1, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0]
-];
-
-const queenThreat = function(arr){
-  let queens = [];
-  let threat = false;
-
-  for (let i = 0; i < arr.length; i++){
-    for (let j = 0; j < arr[i].length; j++){
-      if (arr[i][j]===1){
-        queens.push([i, arr[i].indexOf(1)])
-      }
-    }
-  }
-
-  let queenOne = queens[0]; // [0, 5]
-  let queenTwo = queens[1]; // [5, 0]
-
-
-      // ULTIMATELY return true or false
-  return queens;
-}
-
-console.log(queenThreat(arr));
-
 /* 
-let whiteQueen = [0, 5];
-let blackQueen = [5, 0];
-
-const queenThreat = function(){
-  // NEXT TO DO: loop through arr of array (square loop) if 1 return indexOf to array of (two) arrays? rename queenOne = arr[0], queenTwo = arr[1]
-
-}
-
-console.log(queenThreat(whiteQueen, blackQueen));
-
-whiteQueen = [0, 0];
-blackQueen = [5, 7];
-
-console.log(queenThreat(whiteQueen, blackQueen)); 
-*/
-
-/* 
-let queenOne = whiteQueen;
-let queenTwo = blackQueen;
-let answer = false;
+let arr = ["s", "r", "o", "o", "t", "s"];
 
 for (let i = 0; i < 8; i++){
-  console.log([queenTwo[0], queenOne[0]+i, queenTwo[1], queenOne[1]+i])
-  console.log([queenTwo[0], queenOne[0]-i, queenTwo[1], queenOne[1]-i])
-  console.log([queenTwo[0], queenOne[0]+i, queenTwo[1], queenOne[1]-i])
-  console.log([queenTwo[0], queenOne[0]-i, queenTwo[1], queenOne[1]+i])
-  
-  if ((queenTwo[0] === queenOne[0]+i) && (queenTwo[1] === queenTwo[1]+i)) {
-    console.log([queenTwo[0], queenOne[0]+i, queenTwo[1], queenOne[1]+i])
-    answer = true;
-  } else if ((queenTwo[0] === queenOne[0]-i) && (blackQueen[1] === queenTwo[1]-i)) {
-    console.log([queenTwo[0], queenOne[0]-i, queenTwo[1], queenOne[1]-i])
-    answer = true;
-  }  else if ((queenTwo[0] === queenOne[0]+i) && (queenTwo[1] === queenTwo[1]-i)) {
-    console.log([queenTwo[0], queenOne[0]+i, queenTwo[1], queenOne[1]-i])
-    answer = true;
-  } else if ((queenTwo[0] === queenOne[0]-i) && (blackQueen[1] === queenTwo[1]+i)) {
-    console.log([queenTwo[0], queenOne[0]-i, queenTwo[1], queenOne[1]+i])
-    answer = true;
-  } 
+  if(arr[i] === undefined){
+    console.log("worked")
+    i++
+  } else {
+    console.log(arr[i]);
+  }
 }
-return answer;
  */
+
+squareDims = 4;
+
+let arr =[
+["i", "f", "m", "a"], 
+["m", "e", "a", "n"], 
+["s", "r", "o"]
+]
+
+    // arr[x/j][y/i] ex. arr[x0][x0], arr[x1][y0], arr[x2][y0], … at arr[x3][y4] get undefined
+
+let finalArr = [];
+
+for (let y = 0; y<squareDims; y++){
+  let intArr = [];
+  for (let x = 0; x<arr.length; x++){
+    if (arr[x][y] === undefined) {y++} else {
+      intArr.push(arr[x][y])
+    } 
+  }
+  finalArr.push(intArr);
+}
+
+console.log(finalArr);
